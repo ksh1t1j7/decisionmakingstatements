2a0c2cbda751e3bf5c251bebcd69303474cbfcdb
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-08-06 15:21:25 +0530

    Create evenorodd.cpp

---
+++ evenorodd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+
+    if(a%2 == 0)
+    {
+        cout<<"It is an even number.";
+    }
+    else
+    {
+        cout<<"It is an odd number.";
+    }
+}
+/*
+Enter the number: 5
+It is an odd number.
+
+=== Code Execution Successful ===
+*/
